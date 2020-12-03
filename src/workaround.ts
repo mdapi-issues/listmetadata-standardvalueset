eb8a09726c94de7e08d3430c75f811a1ed66c9de
Matthias Rolke <mr.amtrack@gmail.com>
2020-12-03 12:18:42 +0100

    docs: add note about StandardValueSet id

--- src/workaround.ts
+++ src/workaround.ts
@@ -41,6 +41,15 @@ export default async function listStandardValueSets(
       createdDate: '',
       fileName: `standardValueSets/${standardValueSetName}.standardValueSet`,
       fullName: standardValueSetName,
+      // all StandardValueSets share the same id:
+      // $ sfdx force:data:soql:query -t -q "SELECT Id FROM StandardValueSet WHERE MasterLabel IN ('AccountRating', 'FiscalYearQuarterName', 'LeadSource')"
+      // Querying Data... done
+      // ID
+      // ──────────────────
+      // 000000000000000AAA
+      // 000000000000000AAA
+      // 000000000000000AAA
+      // Total number of records retrieved: 3.
       id: '',
       lastModifiedById: '',
       lastModifiedByName: '',
