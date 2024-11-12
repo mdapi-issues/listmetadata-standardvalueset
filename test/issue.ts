6f953404bdf80ae3dec4087af5f1b6d6f8a8772f
Matthias Rolke <mr.amtrack@gmail.com>
2024-11-12 19:44:09 +0100

    feat: ESM
    BREAKING CHANGE: This package is now an ES module

--- test/issue.ts
+++ test/issue.ts
@@ -4,6 +4,6 @@ import type { Connection } from "@salesforce/core";
 export async function listStandardValueSets(
   conn: Connection
 ): Promise<Array<FileProperties>> {
-  const fileProperties = await conn.metadata.list({ type: 'StandardValueSet' });
+  const fileProperties = await conn.metadata.list({ type: "StandardValueSet" });
   return fileProperties;
 }
--- test/workaround.e2e-spec.ts
+++ test/workaround.e2e-spec.ts
@@ -1,16 +1,16 @@
-import { Org } from '@salesforce/core';
-import { expect } from 'chai';
-import { listStandardValueSets } from '../src/workaround';
+import { Org } from "@salesforce/core";
+import { expect } from "chai";
+import { listStandardValueSets } from "../src/workaround.js";
 
-describe('workaround', function () {
+describe("workaround", function () {
   this.slow(5000);
   this.timeout(20000);
-  it('adds all available StandardValueSets', async () => {
+  it("adds all available StandardValueSets", async () => {
     const org = await Org.create({});
     const conn = org.getConnection();
     const fileProperties = await listStandardValueSets(conn);
-    expect(fileProperties.find((item) => item.fullName === 'CaseOrigin'));
-    expect(fileProperties.find((item) => item.fullName === 'CareItemStatus2'))
+    expect(fileProperties.find((item) => item.fullName === "CaseOrigin"));
+    expect(fileProperties.find((item) => item.fullName === "CareItemStatus2"))
       .to.be.undefined;
   });
 });
